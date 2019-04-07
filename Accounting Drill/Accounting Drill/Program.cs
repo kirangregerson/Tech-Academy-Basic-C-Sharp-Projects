3263be49b7c5bf703e2dbf1674fd1f6bb2f36bf3
kirangregerson <kirangregerson@gmail.com>
2019-04-07 10:11:59 -0700

    Commit of Two drills
    Two projects including the "Accounting" console print drill

---
+++ Accounting Drill/Accounting Drill/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Accounting_Drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems ");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
