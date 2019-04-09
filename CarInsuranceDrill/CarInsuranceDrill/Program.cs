1abc643f5bf22004b506fee7f025cee2c50b6b16
kirangregerson <kirangregerson@gmail.com>
2019-04-09 13:32:38 -0700

    Commit of Car Insurance Approval Drill

---
+++ CarInsuranceDrill/CarInsuranceDrill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarInsuranceDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Car Insurance Approval Program");
+            Console.WriteLine("What Is Your Age?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have You Ever Had a DUI? (True or False)");
+            bool haveDUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How Many Speeding Tickets do you Have?");
+            int numTickets = Convert.ToInt16(Console.ReadLine());
+            bool isQualified = age > 15 && haveDUI == false && numTickets < 3;
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(isQualified);
+            Console.Read();
+        }
+    }
+}
