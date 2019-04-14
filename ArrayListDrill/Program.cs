ffef1576dc553c64a6e69a7612294dd93bb54f14
kirangregerson <kirangregerson@gmail.com>
2019-04-14 13:44:24 -0700

    Commit of the ArraysAndLists and ArrayListDrill projects

---
+++ ArrayListDrill/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayListDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] stringArray = new string[5];
+            stringArray[0] = "Mozambique";
+            stringArray[1] = "Mauritania";
+            stringArray[2] = "Malawi";
+            stringArray[3] = "Morocco";
+            stringArray[4] = "Mali";
+
+            int[] intArray = new int[] {1, 1, 2, 3, 5, 8, 13};
+            List<string> stringList = new List<string> {"Alif", "Hamza", "Baa", "Taa", "Thaa",  };
+
+            Console.WriteLine("Please input a value between 1 and 5 to receive the string at that position");
+            int stringArrayIndex = Convert.ToInt16(Console.ReadLine());
+            if (stringArrayIndex > 5)
+            {
+                Console.WriteLine("Please input a value between 1 and 5");
+                stringArrayIndex = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine(stringArray[stringArrayIndex-1]);
+            Console.WriteLine("Please input a value between 1 and 7 to receive the integer at that position");
+            int intArrayIndex = Convert.ToInt16(Console.ReadLine());
+            if (intArrayIndex > 7)
+            {
+                Console.WriteLine("Please input a value between 1 and 7");
+                intArrayIndex = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine(intArray[stringArrayIndex - 1]);
+            Console.WriteLine("Please input a value between 1 and 5 to receive the integer at that position");
+            int stringListIndex = Convert.ToInt16(Console.ReadLine());
+            if (stringListIndex > 5)
+            {
+                Console.WriteLine("Please input a value between 1 and 5");
+                stringListIndex = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.WriteLine(stringList[stringListIndex - 1]);
+            Console.Read();
+        }
+    }
+}
---
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numArray = new int[5];
+            numArray[0] = 5;
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 300;
+            numArray[4] = 5000;
+
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+            Console.WriteLine(intList[0]);
+            Console.Read();
+        }
+    }
+}
