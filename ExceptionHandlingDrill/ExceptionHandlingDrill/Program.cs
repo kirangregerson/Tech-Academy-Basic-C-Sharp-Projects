7d2df8b614d53d6f6948d53da9c21c3c801b855b
kirangregerson <kirangregerson@gmail.com>
2019-04-20 15:13:33 -0700

    Commit of the Exception Handling Drill

---
+++ ExceptionHandlingDrill/ExceptionHandlingDrill/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandlingDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                List<int> intList = new List<int> { 230, 45, 98, 75, 14 };
+                Console.WriteLine("Input a number to divide by");
+                int divisor = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Results:");
+                foreach (int dividend in intList)
+                {
+                    Console.WriteLine(dividend / divisor);
+                }
+                Console.ReadLine();
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Cannot divide by a string");
+            }
+            Console.WriteLine("Program has passed the try/catch block. Press enter.");
+            Console.ReadLine();
+        }
+    }
+}
+
