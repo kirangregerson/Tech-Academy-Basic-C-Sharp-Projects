cda43ef96b2d4f5cd670128ae3a65d101b3c7e2e
kirangregerson <kirangregerson@gmail.com>
2019-04-21 14:20:36 -0700

    Commit of the Method Drill 3 (Optional Parameters)

---
+++ MethodDrill3/MethodDrill3/Operator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    public class Operator
+    {
+        public int Multiply(int x, int y = 2)
+        {
+            return (x * y);
+        }
+    }
+}
---
+++ MethodDrill3/MethodDrill3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operator Operator = new Operator();
+            Console.WriteLine("Input an integer value");
+            int x = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Input an integer value to multiply by (you may leave this field empty, which defaults to 2)");
+
+            try
+            {
+                int y = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine(Operator.Multiply(x, y));
+            }
+            catch
+            {
+                Console.WriteLine(Operator.Multiply(x));
+            }
+            Console.ReadLine();
+        }
+    }
+}
