4f5c7548f59571349a358a74118ea97d9ced889d
kirangregerson <kirangregerson@gmail.com>
2019-04-21 13:31:13 -0700

    Commit of the Second Method Drill (Overloading)

---
+++ MethodDrill2/MethodDrill2/Operator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill2
+{
+    public class Operator
+    {
+        public int Operation(int x)
+        {
+            return x * 2;
+        }
+        public int Operation(double x)
+        {
+            return (int)(x * (x + 10));
+        }
+        public int Operation(string x)
+        {
+            try
+            {
+                int y = Convert.ToInt32(x);
+                return (y * y) / 3;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ MethodDrill2/MethodDrill2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operator Operate = new Operator();
+            Console.WriteLine(Operate.Operation(12));
+            Console.WriteLine(Operate.Operation(32.76));
+            Console.WriteLine(Operate.Operation("6"));
+            Console.ReadLine();
+        }
+    }
+}
