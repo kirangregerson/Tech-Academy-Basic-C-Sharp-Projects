d369547465871bd4a5137ffafbf56201f3380ecc
kirangregerson <kirangregerson@gmail.com>
2019-04-21 10:32:09 -0700

    Commit of the Class Drill

---
+++ ClassDrill/Operation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    public class Operation
+    {
+        public int AddThree(int x)
+        {
+            return (x + 3);
+        }
+        public int SubtractThree(int x)
+        {
+            return (x - 3);
+        }
+        public int MultiplyThree(int x)
+        {
+            return (x * 3);
+        }
+    }
+}
---
+++ ClassDrill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+
+            Console.WriteLine("Input an Integer to add to 3");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int output = operation.AddThree(input);
+            Console.WriteLine("Output is " + output);
+
+            Console.WriteLine("Input an Integer to subtract 3 from");
+            input = Convert.ToInt32(Console.ReadLine());
+            output = operation.SubtractThree(input);
+            Console.WriteLine("Output is " + output);
+
+            Console.WriteLine("Input an Integer to add to 3");
+            input = Convert.ToInt32(Console.ReadLine());
+            output = operation.MultiplyThree(input);
+            Console.WriteLine("Output is " + output);
+
+            Console.ReadLine();
+        }
+    }
+}
