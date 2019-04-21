31b6eb3ede490466cff16a49eb76ad413c72a847
kirangregerson <kirangregerson@gmail.com>
2019-04-21 11:09:10 -0700

    Commit of the Method Drill

---
+++ MethodDrill/MethodDrill/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+{
+    public class Operation
+    {
+        public void Operate(int x, int y)
+        {
+            int z = (10 * x) - 3;
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ MethodDrill/MethodDrill/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation Operator = new Operation();
+            Operator.Operate(3, 28);
+            Operator.Operate(x : 3, y : 28);
+            Console.ReadLine();
+        }
+    }
+}
