0efd3e59f52bd2c6ce84f810bbf728ffb037221f
kirangregerson <kirangregerson@gmail.com>
2019-04-23 11:15:07 -0700

    Commit of the Method 4 Drill

---
+++ MethodDrill4/MethodDrill4/Divider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill4
+{
+    public class Divider
+    {
+        public void Halve(int x)
+        {
+            Console.WriteLine(x/2);
+        }
+        public static void Multiply(out int x, out int y)
+        {
+            Console.WriteLine("Enter the first of two values to multiply");
+            x = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter the second of two values to multiply");
+            y = Convert.ToInt16(Console.ReadLine()); 
+        }
+        public static void Multiply(out int z)
+        {
+            Console.WriteLine("Enter a value to multiply (by 2)");
+            z = Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
---
+++ MethodDrill4/MethodDrill4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divider divider = new Divider();
+            Console.WriteLine("Input an integer to divide by two");
+            int input = Convert.ToInt16(Console.ReadLine());
+            divider.Halve(input);
+            int x, y, z;
+            Divider.Multiply(out x, out y);
+            Console.WriteLine(x * y);
+            Divider.Multiply(out z);
+            Console.WriteLine(z*2);
+        }
+    }
+}
