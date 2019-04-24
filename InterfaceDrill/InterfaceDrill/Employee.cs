b74197752679ac93115f1c6e8ce25062cc36ce96
kirangregerson <kirangregerson@gmail.com>
2019-04-24 15:56:08 -0700

    Commit of the Interface Drill

---
+++ InterfaceDrill/InterfaceDrill/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDrill
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("This Employee Has Quit.");
+        }
+    }
+}
---
+++ InterfaceDrill/InterfaceDrill/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employee = new Employee();
+            employee.Quit();
+            Console.ReadLine();
+        }
+    }
+}
