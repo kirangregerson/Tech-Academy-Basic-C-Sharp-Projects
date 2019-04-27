ac3e4c85ffbb389745d12b0b0d9ee1c12d35e82a
kirangregerson <kirangregerson@gmail.com>
2019-04-27 12:27:37 -0700

    Initial Commit of the Enum Drill

---
+++ EnumDrill/EnumDrill/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week (capitalized)");
+                Enum.Parse(typeof(DaysOfWeek), Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("That is not a valid day of the week (or is not capitalized)");
+            }
+            Console.ReadLine();
+        }
+        public enum DaysOfWeek
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday          
+        }
+    }
+}
