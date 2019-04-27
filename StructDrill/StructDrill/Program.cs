9047fe589a4879ca503cf93bc0bb458b9cd38486
kirangregerson <kirangregerson@gmail.com>
2019-04-27 14:48:54 -0700

    Initial Commit of the Struct Drill

---
+++ StructDrill/StructDrill/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.amount = 80.794m;
+            Console.WriteLine(num1.amount);
+            Console.ReadLine();
+        }
+    }
+}
