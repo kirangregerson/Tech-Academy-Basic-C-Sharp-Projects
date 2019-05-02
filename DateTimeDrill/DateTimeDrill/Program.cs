bed9e0fa99fcb14e13f4d4c9bc47a8a0f80ac641
kirangregerson <kirangregerson@gmail.com>
2019-05-02 11:16:22 -0700

    Initial Commit of the Date/Time Drill

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nowTime = DateTime.Now;
+            Console.WriteLine(nowTime);
+            Console.WriteLine("Input an integer");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            DateTime thenTime = nowTime.AddHours(hours);
+            Console.WriteLine(thenTime);
+            Console.ReadLine();
+        }
+    }
+}
