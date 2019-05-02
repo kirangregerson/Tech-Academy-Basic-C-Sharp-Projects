2f1e09a23d07024bdf92e1618ea8d1e5696a777a
kirangregerson <kirangregerson@gmail.com>
2019-05-02 10:46:51 -0700

    Initial Commit of the File Writer Drill

---
+++ FileWriterDrill/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWriterDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            string input = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\kiran\myProjects\Tech-Academy-Basic-C-Sharp-Projects\FileWriterDrill\log.txt", input);
+            string text = File.ReadAllText(@"C:\Users\kiran\myProjects\Tech-Academy-Basic-C-Sharp-Projects\FileWriterDrill\log.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
