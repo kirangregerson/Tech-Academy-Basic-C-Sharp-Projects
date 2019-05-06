5964369f6f543412390b2977e63d77727eb9e7da
kirangregerson <kirangregerson@gmail.com>
2019-05-06 13:33:09 -0700

    Intial Commit of the Additional Features Drill (using constructor chaining, contants, and the 'var' keyword)

---
+++ FeaturesDrill/Location.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeaturesDrill
+{
+    public class Location
+    {
+        public Location(string address) : this(address, "Generic Brand Retail Store")
+        {
+        }
+        public Location(string address, string name)
+        {
+            Address = address;
+            Name = name;
+        }
+        public string Address { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ FeaturesDrill/program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeaturesDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string address = "211 85th Street";
+            var newLocation = new Location(address);
+        }
+    }
+}
