6dc7e73eea5ff4633234bc6444796a5d1938be50
kirangregerson <kirangregerson@gmail.com>
2019-05-07 06:03:53 -0700

    Intial Commit of the Exception Handling Drill

---
+++ ExceptionHandlingContDrill/ExceptionHandlingContDrill/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingContDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input your age");
+            uint age;
+            try
+            {
+                age = Convert.ToUInt32(Console.ReadLine());
+                Console.WriteLine(DateTime.Now.Year - age);
+                Console.ReadLine();
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("input cannot be negative");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error");
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
